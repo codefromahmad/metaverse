bee8b91a085e32f8ff5328127c971098f0e6050c
Arsalan Ahmad <you@example.com>
2023-01-30 03:35:12 +0500

    Done with GetStarted,WhatsNew,World and Insight sections

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -22,7 +22,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+      <div className="absolute bottom-0 h-[70%] md:h-1/2 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
@@ -32,10 +32,10 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
             className="w-1/2 h-1/2 object-contain"
           />
         </div>
-        <p className="font-normal md:block hidden text-[16px] leading-[20.16px] text-white uppercase">
+        <p className="font-normal lg:block hidden text-[16px] leading-[20.16px] text-white uppercase">
           Enter Metaverse
         </p>
-        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+        <h2 className="-mt-[10px] lg:mt-[24px] font-semibold lg:text-[32px] text-[24px] text-white">
           {title}
         </h2>
       </div>
